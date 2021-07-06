916c14ba44eb76bacc5e837ae3f6f8591f4c0947
Samuel Weibel <samuel.weibel@gmail.com>
2021-07-06 18:06:44 +0200

    Added more integration tests

--- test/translate-adapter.test.ts
+++ test/translate-adapter.test.ts
@@ -6,11 +6,14 @@ import rimraf from "rimraf";
 import { promisify } from "util";
 import {
 	handleToJsonCommand,
+	handleToWordsCommand,
+	handleTranslateCommand,
 	setDirectories,
 } from "../src/translate-adapter-handlers";
 
 async function runTranslation(
 	name: string,
+	ignoreAdmin: boolean,
 	commandHandler: () => Promise<void>,
 ) {
 	const baseDir = path.resolve(__dirname, "data", name);
@@ -19,36 +22,83 @@ async function runTranslation(
 	await promisify(rimraf)(outputDir);
 	await copy(inputDir, outputDir);
 
+	const adminDir = path.join(outputDir, "admin");
 	await setDirectories({
 		ioPackage: path.join(outputDir, "io-package.json"),
-		admin: path.join(outputDir, "admin"),
+		admin: adminDir,
+		base: ignoreAdmin ? [] : undefined, // don't do admin translations if the directory doesn't exist
 	});
 	await commandHandler();
 
 	const expectedDir = path.join(baseDir, "expected");
 	const result = await dircompare.compare(expectedDir, outputDir, {
+		compareContent: true,
 		compareFileAsync:
 			dircompare.fileCompareHandlers.lineBasedFileCompare.compareAsync,
+		ignoreLineEnding: true,
+		ignoreEmptyLines: true,
 	});
 
-	if (!result.same) {
-		console.log(result);
-	}
-	expect(result.same, "Directories are not the same").to.be.true;
+	const differences = result.diffSet
+		?.filter((d) => d.state !== "equal")
+		.map((d) => {
+			switch (d.state) {
+				case "left":
+					return `- ${path.join(d.relativePath, d.name1 || "?")}`;
+				case "right":
+					return `+ ${path.join(d.relativePath, d.name2 || "?")}`;
+				default:
+					return `! ${path.join(d.relativePath, d.name2 || "?")}`;
+			}
+		})
+		.map((d) => `          ${d}`)
+		.join("\n");
+	expect(
+		result.same,
+		`Directories "expected" and "output" are different:\n${differences}`,
+	).to.be.true;
 }
 
 describe("translate-adapter translate", () => {
-	// TODO
+	it("translates English to all other languages if they don't exist", () => {
+		return runTranslation(
+			"new-translations",
+			false,
+			handleTranslateCommand,
+		);
+	});
+	it("translates English to all other languages only if they don't exist already", () => {
+		return runTranslation(
+			"changing-translations",
+			false,
+			handleTranslateCommand,
+		);
+	});
+	it("translates io-package.json correctly", () => {
+		return runTranslation(
+			"translate-io-package",
+			true,
+			handleTranslateCommand,
+		);
+	});
 });
 
 describe("translate-adapter to-json", () => {
 	it("generates new JSON files if they don't exist", () => {
-		return runTranslation("no-json-yet", handleToJsonCommand);
+		return runTranslation("no-json-yet", false, handleToJsonCommand);
+	});
+	it("updates JSON files if new strings were added to words.js", () => {
+		return runTranslation("update-json", false, handleToJsonCommand);
 	});
 });
 
 describe("translate-adapter to-words", () => {
-	// TODO
+	it("generates a new words.js if it doesn't exist", () => {
+		return runTranslation("no-words-yet", false, handleToWordsCommand);
+	});
+	it("updates words.js if new strings were added to the JSON files", () => {
+		return runTranslation("update-words", false, handleToWordsCommand);
+	});
 });
 
 describe("translate-adapter all", () => {
